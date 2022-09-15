e3737609ff635e2dffeadca1cff3d18a1dda22d2
MrKaplanP <pastphantom3@gmail.com>
2022-09-15 03:31:10 +0300

    Adding Logo and configured manifest

---
+++ app/src/main/java/com/jdi/jdiradio/callbacks/CallbackConfig.java
@@ -0,0 +1,17 @@
+package com.jdi.jdiradio.callbacks;
+
+import com.jdi.jdiradio.models.Ads;
+import com.jdi.jdiradio.models.Radio;
+import com.jdi.jdiradio.models.Settings;
+import com.jdi.jdiradio.models.Social;
+
+import java.util.ArrayList;
+
+public class CallbackConfig {
+
+    public ArrayList<Radio> radio = new ArrayList<>();
+    public ArrayList<Settings> settings = new ArrayList<>();
+    public ArrayList<Ads> ads = new ArrayList<>();
+    public ArrayList<Social> socials = new ArrayList<>();
+
+}
---
+++ app/src/main/java/com/jdi/jdiradio/models/Social.java
@@ -0,0 +1,11 @@
+package com.jdi.jdiradio.models;
+
+import java.io.Serializable;
+
+public class Social implements Serializable {
+
+    public String social_name;
+    public String social_icon;
+    public String social_url;
+
+}
